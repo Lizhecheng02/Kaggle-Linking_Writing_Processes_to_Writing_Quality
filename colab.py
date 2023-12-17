beae0c25efa81828eda138692618ff8b09598891
unknown <2569618533@qq.com>
2023-12-17 11:57:08 -0800

    colab command

---
+++ colab.py
@@ -0,0 +1,6 @@
+!pip install kaggle
+!mkdir ~/.kaggle
+!cp kaggle.json ~/.kaggle/
+!chmod 600 ~/.kaggle/kaggle.json
+!kaggle competitions download -c linking-writing-processes-to-writing-quality
+!unzip linking-writing-processes-to-writing-quality.zip
